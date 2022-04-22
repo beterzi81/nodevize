20f12cbbf0f2b212237db1cd220358f3ac266850
beterzi81 <beterzi81@gmail.com>
2022-04-22 09:41:39 +0300

    9:40 commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,34 @@
-
-
+// const geocode = require('./geocode')
+// const forecast = require('./forecast')
+const express = require('express')
 const port = process.env.PORT || 3000
+const app=express()
+
+
+
+app.get('/',(req,res)=>{
+    res.send('index.html')
+})
+
+app.get('/test_text',(req,res)=>{
+    res.send("18360859002")
+})
+
+app.get('/test_html',(req,res)=>{
+    res.send("<p>Burak Emre Terzi</p>")
+})
+
+app.get('/test_json',(req,res)=>{
+    res.send()
+})
+
+app.get('/test_geocode',(req,res)=>{
+    res.send()
+})
+
+app.get('/test_weather',(req,res)=>{
+    res.send()
+})
 
 
 
