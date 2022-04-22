7d7b82dacc778359730e9b790a8d214003ac3be6
beterzi81 <beterzi81@gmail.com>
2022-04-22 09:59:34 +0300

    10:00 commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,34 +1,39 @@
 // const geocode = require('./geocode')
 // const forecast = require('./forecast')
 const express = require('express')
-const port = process.env.PORT || 3000
-const app=express()
 
+const port = process.env.PORT || 3000
+const app = express();
+app.use(express.json());
 
 
 app.get('/',(req,res)=>{
-    res.send('index.html')
-})
+    res.send('sa');
+});
 
 app.get('/test_text',(req,res)=>{
-    res.send("18360859002")
-})
+    res.send("18360859002");
+});
 
 app.get('/test_html',(req,res)=>{
-    res.send("<p>Burak Emre Terzi</p>")
-})
+    res.send("<p>Burak Emre Terzi</p>");
+});
 
 app.get('/test_json',(req,res)=>{
-    res.send()
-})
+    res.send({ad: 'Burak Emre',soyad: 'Terzi',numara: '18360859002'});
+});
 
 app.get('/test_geocode',(req,res)=>{
-    res.send()
-})
+    res.send();
+});
 
 app.get('/test_weather',(req,res)=>{
-    res.send()
-})
+    res.send();
+});
+
+app.listen(port, () => {
+    console.log("server is running on "+ port);
+});
 
 
 
